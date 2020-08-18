bb4cbaaa21603b2eeb68d44917be83f48f093640
BraulioPicon <brauliopicon96@gmail.com>
2020-08-17 20:22:24 -0700

    agregando libreria de body-parse en server.js y la de cors porque me provoco, por si con este ejemplo se hace el ejemplo en kubernetes ya tenerla

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const routes = require('./Services/routes');
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
 const app = express();
 
+app.use(bodyParser.json());
+
 app.listen(PORT, HOST);
+
+app.use(cors());
+
 console.log(`Running on http://${HOST}:${PORT}`);
 
 routes.assignRoutes(app);
