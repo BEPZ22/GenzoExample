fb31fe9f23abff76b7812abc21a59c680ca353c4
Sergio <sagg_301@outlook.com>
2020-08-17 19:44:01 -0700

    wait for it

--- Database/mySqlDB.js
+++ Database/mySqlDB.js
@@ -2,18 +2,17 @@ const { Sequelize, DataTypes, Model } = require('sequelize');
 const userModel = require('../Model/User');
 
 const sequelize = new Sequelize('DockerBraulio', 'root', '1234', {
-    host: '0.0.0.0',
-    port: 3306,
-    dialect:'mysql'
-  });
+    host: 'db',
+    // port: 3306,
+    dialect: 'mysql'
+});
 
-const User = userModel(sequelize,Sequelize)
+const User = userModel(sequelize, Sequelize)
 
-sequelize.sync({ force: false }).then(()=> {
+sequelize.sync({ force: false }).then(() => {
     console.log("Tabla creadas")
 })
 
 module.exports = {
     User
 }
- 
